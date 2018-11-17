766c9a821540e039ab9b165094c60b5fbebaf834
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-11-16 23:33:25 -0800

    clean structure

--- src/DriverTasks.cpp
+++ src/DriverTasks.cpp
@@ -28,7 +28,7 @@ void FlywheelControlTask(void*)
       for(int FlywheelPower = 0;  FlywheelPower < 127 && flywheelOn; FlywheelPower+= 7)
       {
         m_Flywheel.move(FlywheelPower);
-        pros::delay (50);
+        pros::delay(50);
       }
       while(flywheelOn) {pros::delay(20);}
     }
@@ -53,84 +53,71 @@ void FlywheelControlTask(void*)
 
       if(simpleControlTaskOn)
       {
+
         if(JoystickMain.get_digital(DIGITAL_R2))      //Intake
         {
           m_Intake.move(-115);
         }
-
         else if(JoystickMain.get_digital(DIGITAL_R1)) //Intake and Indexer
-
         {
           m_Indexer.move(-115);
           m_Intake.move(-115);
         }
-
         else if(JoystickMain.get_digital(DIGITAL_L2))//Intake and Indexer Reverse
-
         {
           m_Indexer.move(115);
           m_Intake.move(115);
         }
+        else
+        {
+          m_Indexer.move(0);
+          m_Intake.move(0);
+        }
 
-        else if (JoystickMain.get_digital(DIGITAL_A))//On Flywheel
+        if(JoystickMain.get_digital(DIGITAL_A))//On Flywheel
         {
           flywheelOn = true;
         }
-        else if (JoystickMain.get_digital(DIGITAL_B))//Off Flywheel
+        else if(JoystickMain.get_digital(DIGITAL_B))//Off Flywheel
         {
           flywheelOn = false;
         }
 
-        else if (JoystickMain.get_digital(DIGITAL_Y) && arm_position > 350)//Arm Forward
+
+        if (JoystickMain.get_digital(DIGITAL_Y) && arm_position > 350)//Arm Forward
         {
           m_Arm.move(-90);
         }
-
         else if (JoystickMain.get_digital(DIGITAL_X))//Arm Back
         {
           m_Arm.move(90);
         }
-
-        else if(baseControlTaskOn)
-        {
-          Joystickch4 = JoystickMain.get_analog(ANALOG_LEFT_X);
-          Joystickch2 = JoystickMain.get_analog(ANALOG_RIGHT_Y);
-
-          setBasePower(Joystickch4 + Joystickch2, Joystickch4 - Joystickch2);
-        }
-        /*
-
-        else if (JoystickMain.get_digital(DIGITAL_LEFT))//Arm to horiz position
-        {
-          m_Arm.move(115);
-        }
-
-        */
-
         else if (arm_position < arm_limit )       //arm lock
         {
           m_Arm.move(10);
         }
-
-
-
-        else                                        //Motors off
+        else
         {
-          m_Indexer.move(0);
-          m_Intake.move(0);
           m_Arm.move(0);
+        }
+
+
+
+        Joystickch4 = JoystickMain.get_analog(ANALOG_LEFT_X);
+        Joystickch2 = JoystickMain.get_analog(ANALOG_RIGHT_Y);
+
+        setBasePower(Joystickch4 + Joystickch2, Joystickch4 - Joystickch2);
 
         }
+
+pros::delay(20);
       }
 
-      pros::delay(20);
-
       }
-    }
+
 
     void MoveMotors(int rightspeed, int leftspeed)
     {
-
      m_LeftFront.move(rightspeed*1);
      m_RightFront.move(leftspeed*1);
      m_LeftRear.move(rightspeed*1);
