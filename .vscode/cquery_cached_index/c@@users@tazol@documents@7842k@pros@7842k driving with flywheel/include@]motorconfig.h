bbe389f07691d0b3289b65401cb66cb359364d69
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-11-16 23:16:29 -0800

    delete and untrack unwanted files

--- .vscode/cquery_cached_index/c@@users@tazol@documents@7842k@pros@7842k driving with flywheel/include@]motorconfig.h
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-extern pros::Motor m_LeftFront;
-extern pros::Motor m_RightFront;
-extern pros::Motor m_LeftRear;
-extern pros::Motor m_RightRear;
-
-extern pros::Motor m_Flywheel;
