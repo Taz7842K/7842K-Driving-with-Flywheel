44ca7624c27bfcc747a7a9010815b24d7ef78565
Taz7842K <44256695+Taz7842K@users.noreply.github.com>
2018-12-18 00:33:05 -0800

    buh

--- src/Autonomous/auto_6_Skills.cpp
+++ src/Autonomous/auto_6_Skills.cpp
@@ -2,54 +2,48 @@
 #include "MainConfig.h"
 #include "AutoConfig.h"
 
-bool flywheelOn = true;
 
-void autonomous_6()
-{
+
   Motor intake(om_Intake);
   Motor indexer(om_Indexer);
-  okapi::ChassisControllerIntegrated chassis = ChassisControllerFactory::create( {om_leftfront, om_leftrear},{om_rightfront, om_rightrear},
-  AbstractMotor::gearset::green, {WHEEL_DIAMETER, CHASSIS_WIDTH});
 
 
-    chassis.setMaxVelocity(150);
-      pros::delay(100);
+    chassis->setMaxVelocity(150);
+    pros::delay(100);
 
-      flywheelOn = true;
+    flywheelOn = true;
 
-    chassis.moveDistance(47_in);// Hit low flag
-      pros::delay(100);
-    chassis.moveDistance(-32_in);//move backward, in place to shoot flag
-      pros::delay(100);
+    chassis->moveDistance(47_in);// Hit low flag
+    pros::delay(100);
+    chassis->moveDistance(-32_in);//move backward, in place to shoot flag
+    pros::delay(100);
 
-      chassis.setMaxVelocity(100);
-        pros::delay(100);
+    chassis->setMaxVelocity(100);
+    pros::delay(100);
 
-    chassis.turnAngle(-7_deg);   //turn slightly to aim for flag
-      pros::delay(100);
+    chassis->turnAngle(-7_deg);   //turn slightly to aim for flag
+    pros::delay(100);
 
     intake.moveRelative(-1800,200);//run intake to shoot ball
     indexer.moveRelative(-1800,200);//run indexer to shoot ball
-        pros::delay(300);
+    pros::delay(300);
 
-      chassis.turnAngle(7_deg);   //turn back
-            pros::delay(100);
+    chassis->turnAngle(7_deg);   //turn back
+    pros::delay(100);
 
-    chassis.setMaxVelocity(150);
-      pros::delay(100);
+    chassis->setMaxVelocity(150);
+    pros::delay(100);
 
     flywheelOn = false;
 
-      chassis.moveDistance(-39_in);//move backward, in line with platforms
-        pros::delay(200);
-      chassis.turnAngle(-90_deg);//turn to face platforms
-        pros::delay(200);
+    chassis->moveDistance(-40_in);//move backward, in line with platforms
+    pros::delay(200);
+    chassis->turnAngle(-90_deg);//turn to face platforms
+    pros::delay(200);
 
-      chassis.setMaxVelocity(200);
+    chassis->setMaxVelocity(200);
 
-      chassis.moveDistance(60_in * 1.25);//drive onto center platform
-
-
+    chassis->moveDistance(60_in * 1.25);//drive onto center platform
 
 
 
@@ -103,36 +97,38 @@ void autonomous_6()
 
 
 
-/*
+
+
+    /*
     flywheelOn = true;
 
-      chassis.setMaxVelocity(130);
+    chassis->setMaxVelocity(130);
 
     intake.moveVelocity(-100);         //start intake
-      pros::delay(200);
+    pros::delay(200);
 
-    chassis.moveDistance(49_in);      //move forward and flip cap
-      pros::delay(200);
+    chassis->moveDistance(49_in);      //move forward and flip cap
+    pros::delay(200);
 
     intake.moveVelocity(0);           //stop intake
-      pros::delay(200);
+    pros::delay(200);
 
-    chassis.moveDistance(-46_in);     //back up
-      pros::delay(200);
+    chassis->moveDistance(-46_in);     //back up
+    pros::delay(200);
 
-    chassis.turnAngle(-90_deg);       // turn to face flag
-      pros::delay(200);
+    chassis->turnAngle(-90_deg);       // turn to face flag
+    pros::delay(200);
 
     intake.moveVelocity(-100);        // run intake to shoot ball
 
-      pros::delay(1000);
+    pros::delay(1000);
 
-    chassis.moveDistance(49_in);     //move towards middle flag
-      pros::delay(200);
+    chassis->moveDistance(49_in);     //move towards middle flag
+    pros::delay(200);
 
-    chassis.turnAngle(-90_deg);
+    chassis->turnAngle(-90_deg);
 
-    chassis.moveDistance(49_in);
-*/
+    chassis->moveDistance(49_in);
+    */
 
-}
+  }
